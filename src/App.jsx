dd6bb578b6e24258288f835774a61b7ab42592ee
Maria <lagoofy85@gmail.com>
2025-02-04 14:27:35 +0100

    ultimo cambio

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,41 @@
-
-import './App.css'
-
+import "./App.css";
+import { useState } from "react";
 function App() {
-   return (
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  return (
     <>
-    <button className="show-modal">Show modal 1</button>
-    <button className="show-modal">Show modal 2</button>
-    <button className="show-modal">Show modal 3</button>
-    <div className="modal hidden">
-      <button className="close-modal">&times;</button>
-      <h1>I'm a modal window :ojos_de_corazón:</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-    </div>
-    <div className="overlay hidden"></div>
+      <button className="show-modal">Show modal 1</button>
+      <button className="show-modal">Show modal 2</button>
+      <button className="show-modal">Show modal 3</button>
+      {showModal && (
+        <>
+          <div className="modal">
+            <button className="close-modal">&times;</button>
+            {/* eslint-disable-next-line react/no-unescaped-entities */}
+            <h1>I'm a modal window :ojos_de_corazón:</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className="overlay"></div>
+        </>
+      )}
     </>
   );
 }
-export default App
+export default App;
